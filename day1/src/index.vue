069b520f11c4a6d965d88f8fb1cb685864eb99e8
jina <1490493025@qq.com>
2018-03-05 09:45:12 +0800

    xiugai

---
+++ day1/src/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="wrapper">
+    <text class="box1"></text>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld'
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      logo: 'https://gw.alicdn.com/tfs/TB1yopEdgoQMeJjy1XaXXcSsFXa-640-302.png'
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .wrapper {
+    justify-content: center;
+    align-items: center;
+  }
+  .logo {
+    width: 424px;
+    height: 200px;
+  }
+  .greeting {
+    text-align: center;
+    margin-top: 70px;
+    font-size: 50px;
+    color: #41B883;
+  }
+  .message {
+    margin: 30px;
+    font-size: 32px;
+    color: #727272;
+  }
+</style>
